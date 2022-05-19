e512efcc7174e376d9ee02c76f826cf8d5ad9bab
bitumoni <bitu.moni.001@gmail.com>
2022-05-19 04:23:07 -0700

    connectpen

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 //Route::get('/signup', function () {
-//    return 'notun accnt';
+//   return 'notun accnt';
 //});
 
 //Route::get('/user', [UserController::class, 'index']);
