08ef6a245f032142e7d2c9437fc6b353fc366787
bitumoni <bitu.moni.001@gmail.com>
2022-05-22 01:43:00 -0700

    connectpen

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -65,7 +65,7 @@
                                 </div>
                             <script>CKEDITOR.replace('editor')</script>
                             <br>
-                            <center><div class="fs-3 fw-bold text-white-50">Tell your story to the world</div></center>
+                            <center><div class="fs-3 fw-bold text-white-50">Type your story to the world</div></center>
                                 
                             </div>
                             
@@ -75,21 +75,24 @@
 
                             
                             
-                            <center> <div class="fs-3 fw-bold text-white">Username</div></center>
+                            <center> <div class="fs-3 fw-bold text-white">Connect Pen</div></center>
                                 
                             
                                 <div class="input-group mb-2 ">
                                     <input class="form-control" type="text" placeholder="Title" aria-label="Title" aria-describedby="button-newsletter" />
-                                    <button class="btn btn-success" id="button-newsletter" type="button">Post</button>
+                                    
                                 </div>
-                                <input type="text" class="form-control md-2" id="exampleFormControlInput1" placeholder="Keywords">
-                                
-                           
 
+                                <select class="form-select mb-2" aria-label="Default select example">
+                                        <option value="0" disabled="" selected="">Select Category</option><option value="Fantasy">Fantasy</option><option value="Epic Fantasy">Epic Fantasy</option><option value="Science Fiction">Science Fiction</option><option value="Sci-fi Fantasy">Sci-fi Fantasy</option><option value="Dystopian">Dystopian</option><option value="Horror / Thriller">Horror / Thriller</option><option value="Romance">Romance</option><option value="Contemporary">Contemporary</option><option value="Historical">Historical</option>
+                                        <option value="Self-Help">Self-Help</option><option value="Memoir / Biography">Memoir / Biography</option><option value="Faith-based">Faith-based</option><option value="Art">Art</option><option value="History">History</option><option value="Health &amp; Wellness">Health &amp; Wellness</option><option value="Business">Business</option><option value="How-To / Guide Book">How-To / Guide Book</option>
 
+                                </select>
 
-                                
+                                <input type="text" class="form-control mb-2" name="keywords" placeholder="Keywords (quote, lyrics, movie, film script)">
 
+                                <br>
+                                <div class="input-group md-2">
                                                     <div class="form-check form-check-inline">
                                                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male">
                                                         <i class="bi bi-globe text-white">Public</i>
@@ -102,13 +105,20 @@
                                                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="other">
                                                         <i class="bi bi-person-fill text-white">Only me</i>
                                                     </div>
+                                </div>
 
-
-                                
-                                
-                                
                                 <div class="small text-white-50">We care about privacy, and will never share your data.</div>
+                             <center>
+                                    <button class="btn btn-success btn-lg px-4" id="button-newsletter" type="button">Post</button>
+                                    <div class="small text-white-50">------OR------</div>
+                                    
+                                    <button class="btn btn-primary btn-lg px-4" id="button-newsletter" type="button">Send to publisher</button>
+                                
+                            </center>
+                                
                             </div>
+                            
+                            
                         </div>
             </aside>
     
